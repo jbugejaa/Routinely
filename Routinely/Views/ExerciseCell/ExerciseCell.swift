e9f9b914b874cf420b8ce8cf72b89c0627fa051b
jbugejaa <jbugejaa@gmail.com>
2021-08-23 19:10:09 +1000

    Create exercise table cell

---
+++ Routinely/Views/ExerciseCell/ExerciseCell.swift
@@ -0,0 +1,16 @@
+//
+//  ExerciseCell.swift
+//  Routinely
+//
+//  Created by Joshua Bugeja on 23/8/21.
+//
+
+import UIKit
+
+class ExerciseCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
